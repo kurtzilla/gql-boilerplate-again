faa2070dbc00524c005ab81b15296dd165e8231a
Rob <Kurtz>
2019-05-06 17:54:53 -0600

    initial commit from 3_insert_user branch

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { importSchema } from "graphql-import";
+import { GraphQLServer } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import * as path from "path";
+
+import { resolvers } from "./resolvers";
+
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+createConnection().then(() => {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
